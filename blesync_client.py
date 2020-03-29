58ae4533711d3749cd8ededf9d94267f3524c9de
Jan Češpivo <jan.cespivo@gmail.com>
2020-03-29 21:52:35 +0200

    Implementation

--- blesync_client.py
+++ blesync_client.py
@@ -61,21 +61,6 @@ def decode_adv_type(data):
     return data[_ADV_TYPE_FLAGS][0]
 
 
-# def decode_field(payload, adv_type):
-#     i = 0
-#     result = []
-#     while i + 1 < len(payload):
-#         # if payload[i + 1] == adv_type:
-#         result.append(payload[i + 2: i + payload[i] + 1])
-#         i += 1 + payload[i]
-#     return result
-
-
-# def decode_name(payload):
-#     n = decode_field(payload, _ADV_TYPE_NAME)
-#     return str(n[0], "utf-8") if n else ""
-
-
 # def decode_services(payload):
 #     services = []
 #     for u in decode_field(payload, _ADV_TYPE_UUID16_COMPLETE):
