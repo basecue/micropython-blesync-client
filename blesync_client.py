68bd9aa01131aaafcc3472a369c5477926b682d5
Jan Češpivo <jan.cespivo@gmail.com>
2020-05-15 13:51:14 +0200

    Implementation (#3)

--- blesync_client.py
+++ blesync_client.py
@@ -1,5 +1,7 @@
 from collections import namedtuple
+import struct
 
+import bluetooth
 from micropython import const
 
 import blesync
@@ -15,20 +17,12 @@ _ADV_TYPE_UUID128_MORE = const(0x6)
 _ADV_TYPE_APPEARANCE = const(0x19)
 
 
-# 0x00 - ADV_IND - connectable and scannable
-# undirected
-# advertising
-# 0x01 - ADV_DIRECT_IND - connectable
-# directed
-# advertising
-# 0x02 - ADV_SCAN_IND - scannable
-# undirected
-# advertising
-# 0x03 - ADV_NONCONN_IND - non - connectable
-# undirected
-# advertising
-# 0x04 - SCAN_RSP - scan
-# response
+def _next(iterator, default):
+    try:
+        return next(iterator)
+    except StopIteration:
+        return default
+
 
 def _split_data(payload):
     i = 0
@@ -43,83 +37,81 @@ def _split_data(payload):
 
 
 def parse_adv_data(payload):
-    return {
-        d[0]: d[1:]
-        for d in _split_data(payload)
-    }
+    return [(d[0], d[1:]) for d in _split_data(payload)]
+
+
+def _find_adv_data(data, find_adv_type):
+    for adv_type, payload in data:
+        if adv_type == find_adv_type:
+            yield payload
 
 
 def decode_adv_name(data):
-    try:
-        encoded_name = data[_ADV_TYPE_NAME]
-    except KeyError:
-        return ''
-    else:
-        return str(encoded_name, "utf-8")
+    payload = _next(_find_adv_data(data, _ADV_TYPE_NAME), None)
+    if payload:
+        return str(payload, "utf-8")
+    return ''
 
 
-def decode_adv_type(data):
-    try:
-        return data[_ADV_TYPE_FLAGS][0]
-    except KeyError:
-        return None
+def decode_adv_type_flags(data):
+    payload = _next(_find_adv_data(data, _ADV_TYPE_FLAGS), None)
+    if payload:
+        return payload[0]
 
 
-# TODO
-# def decode_services(payload):
-#     services = []
-#     for u in decode_field(payload, _ADV_TYPE_UUID16_COMPLETE):
-#         services.append(bluetooth.UUID(struct.unpack("<h", u)[0]))
-#     for u in decode_field(payload, _ADV_TYPE_UUID32_COMPLETE):
-#         services.append(bluetooth.UUID(struct.unpack("<d", u)[0]))
-#     for u in decode_field(payload, _ADV_TYPE_UUID128_COMPLETE):
-#         services.append(bluetooth.UUID(u))
-#     return services
+def decode_adv_services(data):
+    services = []
+    for payload in _find_adv_data(data, _ADV_TYPE_UUID16_COMPLETE):
+        services.append(bluetooth.UUID(struct.unpack("<h", payload)[0]))
+    for payload in _find_adv_data(data, _ADV_TYPE_UUID32_COMPLETE):
+        services.append(bluetooth.UUID(struct.unpack("<d", payload)[0]))
+    for payload in _find_adv_data(data, _ADV_TYPE_UUID128_COMPLETE):
+        services.append(bluetooth.UUID(payload))
+    return services
+
+
+# TODO ADV_FLAGS
+# _ADV_IND = const(0x00)
+# '''
+# Known as Advertising Indications (ADV_IND), where a peripheral device requests connection to any central device (i.e., not directed at a particular central device).
+# Example: A smart watch requesting connection to any central device.
+# '''
+# _ADV_DIRECT_IND = const(0x01)
+# '''
+# Similar to ADV_IND, yet the connection request is directed at a specific central device.
+# Example: A smart watch requesting connection to a specific central device.
+# '''
+# _ADV_SCAN_IND = const(0x02)
+# '''
+# Similar to ADV_NONCONN_IND, with the option additional information via scan responses.
+# Example: A warehouse pallet beacon allowing a central device to request additional information about the pallet.
+# '''
+# _ADV_NONCONN_IND = const(0x03)
+# '''
+# Non connectable devices, advertising information to any listening device.
+# Example: Beacons in museums defining proximity to specific exhibits.
+# '''
+# _ADV_SCAN_RSP = const(0x04)
+# '''
+# Scan response
+# '''
 
 
 BLEDevice = namedtuple(
     'BLEDevice',
-    ('addr_type', 'addr', 'adv_name', 'adv_type_flags', 'rssi',)
+    ('addr_type', 'addr', 'adv_name', 'adv_type_flags', 'rssi', 'services')
 )
 
-BLEService = namedtuple(
-    'BLEService',
-    ('start_handle', 'end_handle', 'uuid')
-)
 
-_ADV_IND = const(0x00)
-'''
-Known as Advertising Indications (ADV_IND), where a peripheral device requests connection to any central device (i.e., not directed at a particular central device).
-Example: A smart watch requesting connection to any central device.
-'''
-_ADV_DIRECT_IND = const(0x01)
-'''
-Similar to ADV_IND, yet the connection request is directed at a specific central device.
-Example: A smart watch requesting connection to a specific central device.
-'''
-_ADV_SCAN_IND = const(0x02)
-'''
-Similar to ADV_NONCONN_IND, with the option additional information via scan responses.
-Example: A warehouse pallet beacon allowing a central device to request additional information about the pallet. 
-'''
-_ADV_NONCONN_IND = const(0x03)
-'''
-Non connectable devices, advertising information to any listening device.
-Example: Beacons in museums defining proximity to specific exhibits.
-'''
-
-
-def scan():
+def scan(duration_ms, interval_us=None, window_us=None, timeout_ms=None):
     blesync.active(True)
     for addr_type, addr, adv_type, rssi, adv_data in blesync.gap_scan(
-        2000,
-        30000,
-        30000
+        duration_ms, interval_us=interval_us, window_us=window_us, timeout_ms=timeout_ms
     ):
         parsed_data = parse_adv_data(adv_data)
         adv_name = decode_adv_name(parsed_data)
-        adv_type_flags = decode_adv_type(parsed_data)
-
+        adv_type_flags = decode_adv_type_flags(parsed_data)
+        adv_services = decode_adv_services(parsed_data)
         # addr buffer is owned by caller so need to copy it.
         addr_copy = bytes(addr)
         yield BLEDevice(
@@ -128,6 +120,7 @@ def scan():
             adv_name=adv_name,
             adv_type_flags=adv_type_flags,
             rssi=rssi,
+            services=adv_services,
         )
 
 
@@ -233,14 +226,14 @@ class Service:
         for def_handle, value_handle, properties, uuid in blesync.gattc_discover_characteristics(
             conn_handle, start_handle, end_handle
         ):
-            for characteristic in characteristics:  # TODO enumerate
+            for index, characteristic in enumerate(characteristics, start=1):
                 try:
                     characteristic.register(uuid, self, value_handle)
                 except ValueError:
                     continue
                 else:
                     self._characteristics[value_handle] = characteristic
-                    if len(self._characteristics) == len(characteristics):
+                    if index == len(self._characteristics):
                         return
                     break
 
@@ -258,3 +251,6 @@ class Service:
     #     self._assert_active()
     #     self._disconnect_callback[connection] = callback
     #     _ble.gap_disconnect(connection.conn_handle)
+
+
+on_disconnect = blesync.on_peripherial_disconnect
